b1186ab9022122f9e2d2c9753d3ad7d9e679d71b
tanvirtbf <tanver018765@gmail.com>
2025-06-26 15:22:00 +0600

    update

--- components/home/mobile-scanner/components/left/index.jsx
+++ components/home/mobile-scanner/components/left/index.jsx
@@ -5,8 +5,8 @@ import LeftImage from '../../../../../public/png/p55.png'
 const Left = () => {
   return (
     <div className='relative order-1 w-40 sm:w-60 sm:order-1'>
-      <div className='absolute flex items-center justify-center sm:bottom-0 top-[-230px] '>
-        <Image src={LeftImage} alt='Left Image' width={0} height={0} className='w-40 h-[400px] sm:w-60 sm:h-[500px] rounded-xl' />
+      <div className='absolute flex items-center justify-center sm:bottom-0 sm:top-[-230px] top-[-110px] '>
+        <Image src={LeftImage} alt='Left Image' width={0} height={0} className='w-40 h-[280px] sm:w-60 sm:h-[500px] rounded-xl' />
       </div>
     </div>
   )
