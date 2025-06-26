23de1aa0d1da581cb8f594d2e07f5047cd91fdea
tanvirtbf <tanver018765@gmail.com>
2025-06-26 15:14:12 +0600

    update

--- components/home/mobile-scanner/components/middle/components/input/components/InputBox.jsx
+++ components/home/mobile-scanner/components/middle/components/input/components/InputBox.jsx
@@ -5,7 +5,7 @@ const InputBox = () => {
   return (
     <div className="flex p-2 bg-white rounded-xl">
       <GmailIcon className="flex items-center justify-center bg-[#F1F2F7] rounded-md p-2" />
-      <input type="text" placeholder="enter your mail" className="w-full outline-none placeholder:font-normal placeholder:text-sm placeholder:text-[#222222] pl-10" />
+      <input type="text" placeholder="enter your mail" className="w-full outline-none placeholder:font-normal placeholder:text-sm placeholder:text-[#222222] sm:pl-10 pl-4" />
     </div>
   );
 };
