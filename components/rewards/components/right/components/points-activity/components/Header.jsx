0fb1e3041f7fce32317045f65951afa21fd9bde8
tanvirtbf <tanver018765@gmail.com>
2025-07-07 12:46:24 +0600

    bar complete

--- components/rewards/components/right/components/points-activity/components/Header.jsx
+++ components/rewards/components/right/components/points-activity/components/Header.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 
 const Header = () => {
+    const [whichBar, setWhichBar] = useState("all")
+    const handleClick = (state) =>{
+        setWhichBar(state)
+    }
   return (
     <div className='flex items-center gap-6'>
-        <div>All</div>
-        <div>Earned</div>
-        <div>Used</div>
-        <div>Expired</div>
+        
+        <div className={`text-xs  leading-4 cursor-pointer pb-2 ${whichBar === "all" ? "border-b-black border-b-[2px] text-black font-medium" : "border-b-transparent border-b-[2px] text-[#717171] font-normal"}`} onClick={() => handleClick('all')}>All</div>
+        
+        <div className={`text-xs  leading-4 cursor-pointer pb-2 ${whichBar === "earned" ? "border-b-black border-b-[2px] text-black font-medium" : "border-b-transparent border-b-[2px] text-[#717171] font-normal"}`} onClick={() => handleClick('earned')}>Earned</div>
+
+        <div className={`text-xs  leading-4 cursor-pointer pb-2 ${whichBar === "used" ? "border-b-black border-b-[2px] text-black font-medium" : "border-b-transparent border-b-[2px] text-[#717171] font-normal"}`} onClick={() => handleClick('used')}>Used</div>
+
+        <div className={`text-xs  leading-4 cursor-pointer pb-2 ${whichBar === "expired" ? "border-b-black border-b-[2px] text-black font-medium" : "border-b-transparent border-b-[2px] text-[#717171] font-normal"}`} onClick={() => handleClick('expired')}>Expired</div>
+        
     </div>
   )
 }
